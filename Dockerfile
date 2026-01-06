49f216a32f5cbc01397f4a2cb4a2c7ad00e3892c
lohithkumar1327 <lohith1327009@gmail.com>
2026-01-06 17:13:01 +0000

    Fresh start

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+cat <<EOF > Dockerfile
+FROM python:3.9-slim
+WORKDIR /app
+COPY app.py .
+RUN pip install flask
+CMD ["python", "app.py"]
+EOF
---
+++ app1.py
@@ -0,0 +1,11 @@
+cat <<EOF > app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+        return "<h1>Mission Success! Deployed by Lohith's Pipeline.</h1>"
+
+    if __name__ == "__main__":
+            app.run(host='0.0.0.0', port=5000)
+            EOF
